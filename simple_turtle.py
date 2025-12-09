8c495abde3c4b3a4b173afbd2909451dbd6da5d9
alichahardowly <alichahardowly9000893@gmail.com>
2025-12-09 19:24:09 +0330

    simple turtle with python

---
+++ simple_turtle.py
@@ -0,0 +1,11 @@
+import turtle
+t=turtle.Turtle()
+t.pencolor('blue')
+for i in range(50):
+    t.forward(50)
+    t.right(123)
+t.pencolor("red")
+for i in range(6):
+    t.forward(70)
+    t.right(60)
+turtle.done()
